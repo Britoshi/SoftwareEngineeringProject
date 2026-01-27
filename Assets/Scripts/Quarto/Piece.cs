0855a65818726aaf2bc6fed44f152fb58f0d9978
Min Jun Kim <BrianMJK@Gmail.com>
2026-01-26 18:48:30 -0800

    Implemented Drawing using line renderers
    Created Essential pieces such as Board, Piece, Tile, and made a functioning Game Manager along with a drawing system using line renderers

---
+++ Assets/Scripts/Quarto/Piece.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Quarto
+{
+    /// <summary>
+    /// This is to be attached to a game object that represents a piece on the board
+    /// </summary>
+    public class Piece : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
